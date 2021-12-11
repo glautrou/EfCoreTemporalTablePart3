27e11b7367d28b5bbb91647fc21d00287ec4e684
Gilles Lautrou <g.lautrou@outlook.com>
2021-12-11 11:50:44 +0100

    dotnet ef migrations add InitialCreation

---
+++ src/Migrations/20211211104839_Initial.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCoreTemporalTablePart3.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entreprise",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    SysStartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SysEndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValideAu = table.Column<DateTime>(type: "datetime2", nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValideAu")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValideDu"),
+                    ValideDu = table.Column<DateTime>(type: "datetime2", nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValideAu")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValideDu")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entreprise", x => x.Id);
+                })
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "EntrepriseHistorique")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValideAu")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValideDu");
+
+            migrationBuilder.CreateTable(
+                name: "Employe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Prenom = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    EntrepriseId = table.Column<int>(type: "int", nullable: false),
+                    PeriodEnd = table.Column<DateTime>(type: "datetime2", nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart"),
+                    PeriodStart = table.Column<DateTime>(type: "datetime2", nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employe_Entreprise",
+                        column: x => x.EntrepriseId,
+                        principalTable: "Entreprise",
+                        principalColumn: "Id");
+                })
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "EmployeHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employe_EntrepriseId",
+                table: "Employe",
+                column: "EntrepriseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employe")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "EmployeHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.DropTable(
+                name: "Entreprise")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "EntrepriseHistorique")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValideAu")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValideDu");
+        }
+    }
+}
