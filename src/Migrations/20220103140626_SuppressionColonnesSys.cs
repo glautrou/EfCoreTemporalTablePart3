d49425db2e6efae40cfcbf2122f51ce31560f3c2
Gilles Lautrou <g.lautrou@outlook.com>
2022-01-03 15:07:05 +0100

    Clean DB

---
+++ src/Migrations/20220103140626_SuppressionColonnesSys.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCoreTemporalTablePart3.Migrations
+{
+    public partial class SuppressionColonnesSys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
